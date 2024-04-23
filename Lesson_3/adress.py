59b9938905b7e91b6fbecefb4aeba168ff5d556d
Admin <kiseleva.tbo@gmail.com>
2024-04-23 16:54:41 +0300

    HW3

---
+++ Lesson_3/adress.py
@@ -0,0 +1,14 @@
+class Adress():
+    index=''
+    city = ''
+    street = ''
+    house = ''
+    apart = ''
+    def __init__(self, ind, cit, str, h, ap):
+        self.index = ind
+        self.city = cit
+        self.street = str
+        self.house = h
+        self.apart = ap
+
+  
---
+++ Lesson_3/lesson_3_task_1.py
@@ -0,0 +1,16 @@
+from User import User
+my_user=User("Fedya", "Dostoevskiy")
+my_user.bothName()
+my_user.firstName()
+my_user.lastName()
+
+
+
+
+
+
+anna = User("Anna", "pi")
+
+anna.firstName()
+
+
