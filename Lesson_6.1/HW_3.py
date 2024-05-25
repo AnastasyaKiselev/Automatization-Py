effd3343ca96e78e64a6ce2b012dd737052fd4a8
Admin <kiseleva.tbo@gmail.com>
2024-05-25 13:57:35 +0300

    HW6.1

---
+++ Lesson_6.1/HW_3.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://www.saucedemo.com/")
+driver.find_element(By.CSS_SELECTOR, '#user-name').send_keys('standard_user')
+driver.find_element(By.CSS_SELECTOR, '#password').send_keys('secret_sauce')
+driver.find_element(By.CSS_SELECTOR, '#login-button').click()
+driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack').click()
+driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bolt-t-shirt').click()
+driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-onesie').click()
+driver.find_element(By.CSS_SELECTOR, '[data-test="shopping-cart-badge"]').click()
+driver.find_element(By.CSS_SELECTOR, '#checkout').click() 
+driver.find_element(By.CSS_SELECTOR, '#first-name').send_keys("Nastya")
+driver.find_element(By.CSS_SELECTOR, '#last-name').send_keys("Kiseleva")
+driver.find_element(By.CSS_SELECTOR, '#postal-code').send_keys('197348')
+driver.find_element(By.CSS_SELECTOR, '#continue').click() 
+total_label = driver.find_element(By.CSS_SELECTOR, '.summary_total_label').text
+print (total_label)
+
+
