6b45f45a6cd21822d3a013c7dbd1f6520b0c2059
Admin <kiseleva.tbo@gmail.com>
2024-05-26 18:26:26 +0300

    HW6-2

--- Lesson_6.1/HW_2.py
+++ Lesson_6.1/HW_2.py
@@ -7,17 +7,18 @@ from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-driver.find_element(By.CSS_SELECTOR, '#delay').clear()
-driver.find_element(By.CSS_SELECTOR, '#delay').send_keys(45)
-driver.find_element(By.CSS_SELECTOR, '.btn.btn-outline-primary' ).click()
-driver.find_element(By.CSS_SELECTOR, '.operator.btn.btn-outline-success' ).click()
-num = driver.find_elements(By.CSS_SELECTOR, '.btn.btn-outline-primary') #ЛОКАТОР 8!!!!!!!!
-for i in num:
-    if i.text =='8':
-        i.click()     
-driver.find_element(By.CSS_SELECTOR, '.btn.btn-outline-warning' ).click()
-waiting = WebDriverWait(driver, 55).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[style='display: none;']" )))
-#.spinner-border#spinner  [style="display: none;"]
+def test():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    driver.find_element(By.CSS_SELECTOR, '#delay').clear()
+    driver.find_element(By.CSS_SELECTOR, '#delay').send_keys(45)
+    driver.find_element(By.CSS_SELECTOR, '.btn.btn-outline-primary' ).click()
+    driver.find_element(By.CSS_SELECTOR, '.operator.btn.btn-outline-success' ).click()
+    num = driver.find_elements(By.CSS_SELECTOR, '.btn.btn-outline-primary') #ЛОКАТОР 8!!!!!!!!
+    for i in num:
+        if i.text =='8':
+            i.click()     
+    driver.find_element(By.CSS_SELECTOR, '.btn.btn-outline-warning' ).click()
+    waiting = WebDriverWait(driver, 55).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[style='display: none;']" )))
+    result = driver.find_element(By.CSS_SELECTOR, '.screen').text
+    assert int(result)==15
